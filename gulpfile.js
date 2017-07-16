0fa6156fbb3356acaeba9c5769c85a64adb56f8d
huyuan <422018883@qq.com>
2017-07-16 22:57:02 +0800

    安装Less编译运行的环境

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+//导入工具包 require('node_modules里对应模块')
+var gulp = require('gulp'), //本地安装gulp所用到的地方
+    less = require('gulp-less');
+
+//定义一个flex任务（自定义任务名称）
+gulp.task('flex', function () {
+    gulp.src('./less/*.less') //该任务针对的文件
+        .pipe(less()) //该任务调用的模块
+        .pipe(gulp.dest('./styles')); //将会在src/css下生成相应的css文件
+});
+
+gulp.task('default',['flex', 'elseTask']); //定义默认任务 elseTask为其他任务，该示例没有定义elseTask任务
+
+gulp.task('watchLess',function(){
+//监听所有less文件，如果有变化，则执行flex编译方法
+ gulp.watch(['./less/*.less'],['flex']);
+});
